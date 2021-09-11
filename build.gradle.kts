171c69f58009aaa5ed6677a351a09e6db70f9499
Kazuki Mineshima <kazuki.mineshima@best-couple.love>
2021-09-11 14:42:06 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.20-M1"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlinStdlibJdk8)
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,1 @@
+const val kotlinStdlibJdk8: String = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
