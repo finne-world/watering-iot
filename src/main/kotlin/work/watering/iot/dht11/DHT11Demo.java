691ca40f49ebbda9b9a6e25d3cd6171cbd577247
IS13th <k021c6002@gmail.com>
2021-10-15 20:02:57 +0900

    commit

---
+++ src/main/kotlin/work/watering/iot/dht11/DHT11Demo.java
@@ -0,0 +1,22 @@
+package work.watering.iot.dht11;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class DHT11Demo {
+    public static void main(String[] args) throws InterruptedException {
+        DHT11 dht11 = new DHT11(15); // use GPIO pin 15
+
+        while (true) {
+            DHT11Result result = dht11.read();
+
+            if (result.isValid()) {
+                System.out.println("Last valid input: " + new Date());
+                System.out.printf("Temperature: %.1f C\n" , result.getTemperature());
+                System.out.printf("Humidity:    %.1f %%\n", result.getHumidity());
+            }
+
+            TimeUnit.SECONDS.sleep(2);
+        }
+    }
+}
